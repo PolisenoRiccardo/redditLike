de7aca9d838f583e556eeb2a3d4559319d9a1b5d
Poliseno Riccardo <poliseno.riccardo@iisgalvanimi.edu.it>
2024-01-08 12:00:34 +0000

    componente

--- angular-reddit/src/app/app.component.ts
+++ angular-reddit/src/app/app.component.ts
@@ -5,6 +5,14 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'angular-reddit';
+  addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
+    let t = title.value;
+    let v= link.value;
+    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: `+title.value+ ` and link: `+ link.value);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
 }
